a349dda3335d4bee2933ab2f49be1a8d680effca
sanlaelaecho <sanlaelaecho@gmail.com>
2023-05-13 10:40:12 -0500

    GGWP add read me

--- README.md
+++ README.md
@@ -1,12 +1,32 @@
 # Game Name
 Naruto vs Sasuke!!!
+
+### Code Used
+Html/CSS/JavaScript
+
+## Play Here: https://sanlaelaecho.github.io/unit-1-project/
+
 ## Game Description
 Welcome to the Naruto vs Sasuke 2 player game!
 You can choose your location out of 6 famous fighting locations in Naruto.
+![Background Fighting Locations](https://i.imgur.com/4yA848W.png)
 
 ## Game Instruction
 Naruto or Sasuke, who gets to go first is random and then it's a turned based game after that.
 - Player can choose out of 4 spells to attack with the furthest right being the strongest spell.
 - Whichever player's health goes down to 0 or lower dies and looses and the other player wins.
 Now feel free to replay!
+![Naruto vs Sasuke Fight Scene](https://i.imgur.com/vfcX8NI.png)
 
+## Summarized Code Logic
+- Carousel flips through array of different images.
+
+
+## Next Steps
+Add credit for the audio and the Naruto/Sasuke images.
+
+I plan to improve my CSS to better orient the avatars and their animations. Also, add shadows or clouds under the avatars so they look less like they're floating. Improve the animations.
+
+I also plan to try to get my audio linked to a YouTube video's audio and not downloaded or uploaded on GitHub. Also, add audio every time either Naruto or Sasuke uses their attacks. For eg, if Naruto uses Rasengan, he screams Rasengan in audio.
+
+THANK YOU for playing my game!!!
--- app.js
+++ app.js
@@ -252,8 +252,9 @@ const backgroundImage = [
     "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/84dc13b7-a2e7-4b45-83ec-311e72e82900/ddlnx85-af0e161c-6071-4157-b021-b28a58ef275a.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzg0ZGMxM2I3LWEyZTctNGI0NS04M2VjLTMxMWU3MmU4MjkwMFwvZGRsbng4NS1hZjBlMTYxYy02MDcxLTQxNTctYjAyMS1iMjhhNThlZjI3NWEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.eqnsSOdDUvvii_OgzaTYrXRW-Ax0_WDSp9dwhXVdB3Y",
     "https://wallpaperaccess.com/full/3948996.jpg",
     "https://images.start.gg/images/tournament/289797/image-c5c752996c1d06b48c8a90d5dfdacc6a.png?ehk=SqBiJLOchN4v6%2BH%2FTdfVr6eDrBMc3NctYO3YOALgd4o%3D&ehkOptimized=3dT5tdfKHCjhm%2BUOXjWF6xuBAivhlnE62rsqfzU0qRE%3D",
-    "https://static.wikia.nocookie.net/narutofanon/images/f/fc/Ryuhone_Desert.png/revision/latest/scale-to-width-down/1000?cb=20141002162139",
-    "https://static.wikia.nocookie.net/naruto/images/f/f8/Kaguya%27s_Core_Dimension.png/revision/latest/scale-to-width-down/1000?cb=20160901114351"
+    "https://cdn.gamedevmarket.net/wp-content/uploads/20191203145243/a39b5e87402f1e78e58ead9096c0c65f.png",
+    "https://static.wikia.nocookie.net/naruto/images/f/f8/Kaguya%27s_Core_Dimension.png/revision/latest/scale-to-width-down/1000?cb=20160901114351",
+    "https://static.wikia.nocookie.net/narutofanon/images/f/fc/Ryuhone_Desert.png/revision/latest?cb=20141002162139"
 ]
 
 let currentBackground = 0
